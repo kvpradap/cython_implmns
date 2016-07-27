264182a2f88a341c65cc2434d1f1c4b68540c60b
kvpradap <pradap@cs.wisc.edu>
2016-07-26 21:39:41 -0500

    Removed all files

--- cython_implementations/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__author__ = 'Pradap Konda'
-__email__ = 'pradap@cs.wisc.edu'
-__version__ = '0.1.0'
--- docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use Cython Implementations in a project::
-
-    import cython_implementations
